1c2998fbe65741bcdd4994118178c5e0cebc5779
Yunus Emrah Tuncer <yunus.emrah.tuncer@mni.thm.de>
2015-01-25 13:16:34 +0100

    JQuery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 ﻿$(document).ready(function(){
+    //index
     $(".thumbnail").mouseenter(function(){
         $(this).css("opacity","1");
     });
@@ -7,6 +8,12 @@
         $(this).css("opacity","0.5");
     });
     
+    //Impressum
+    $("#imprintAccordion").accordion();
+    
+    
+    
+    //Search
     $("#searchbar").keyup(function(){
         var maxSearch = 59;
         var searchInput = $(this).val().length;
